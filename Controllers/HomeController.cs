ad23e5557cc6dd46c6bb41f8dfdbcfff0ba61de6
JoeriRietjens <45871940+JoeriRietjens@users.noreply.github.com>
2019-11-13 10:17:10 +0100

    13-11-2019

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,22 +4,40 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Unpourd.Models;
+using Logic;
 
 namespace Unpourd.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public IActionResult HomePage()
         {
-            _logger = logger;
+
+            PostManager postManager = new PostManager(); 
+           
+           
+
+
+            return View();
         }
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
             return View();
         }
 
